f84c890a1762be4a87599b01cfd370a4c3257d95
kev <lineus@gmail.com>
2026-03-28 16:51:55 -0400

    adding new headers to export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,4 @@
 export * from './state.svelte'
+export { default as SchemeDatasetHeader } from './SchemeDatasetHeader.svelte';
+export { default as SchemeClassHeader } from './SchemeClassHeader.svelte';
+
