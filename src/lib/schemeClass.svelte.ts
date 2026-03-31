d3060b0cfdcbd5afbfe9b77500041e31bc4e3f34
kev <lineus@gmail.com>
2026-03-31 14:25:31 -0400

    moved class declaration into its own file and removed default option override. I want to revisit it in a way that allows me to reset the value once the class is instantiated.

---
+++ src/lib/schemeClass.svelte.ts
@@ -0,0 +1,52 @@
+import { MediaQuery } from "svelte/reactivity";
+import { on } from "svelte/events";
+import { onDestroy } from "svelte";
+
+export type SystemScheme = 'light' | 'dark';
+export type SiteScheme = 'light' | 'dark' | 'system';
+
+const QRY = '(prefers-color-scheme: dark)';
+
+export class SchemeState {
+  #systemQuery = new MediaQuery(QRY);
+  #system = $derived<SystemScheme>(this.#systemQuery.current ? 'dark' : 'light');
+  #site = $state<SiteScheme>('system');
+  #removeStorageListener:VoidFunction;
+  
+  current = $derived.by(() => {
+      if (this.#site === 'system') {
+        return this.#system;
+      } else {
+        return this.#site;
+      }
+  });
+  
+  constructor(browser:boolean) {
+    if (browser) {
+      this.#site = localStorage.getItem('scheme') as SiteScheme ?? 'system';
+      this.#removeStorageListener = on(window, 'storage', (e: StorageEvent) => {
+        if (e.key === 'scheme') {
+          this.#site = e.newValue as SiteScheme ?? 'system';
+        }          
+      })
+      onDestroy(() => {
+        this.#removeStorageListener();
+      })
+    } else {
+      this.#removeStorageListener = () => {};
+    }
+  }
+
+  set = (v: SiteScheme) => {
+    this.#site = v;
+    localStorage.setItem('scheme', v);
+  }
+
+  get site(): SiteScheme {
+    return this.#site;
+  }
+
+  get system(): SystemScheme {
+    return this.#system;
+  }
+}
--- src/lib/state.svelte.ts
+++ src/lib/state.svelte.ts
@@ -1,32 +1,10 @@
-import { MediaQuery } from "svelte/reactivity";
-import { on } from "svelte/events";
-import { getContext, hasContext, onDestroy, setContext } from "svelte";
+import { getContext, hasContext, setContext } from "svelte";
+import { SchemeState } from './schemeClass.svelte.ts';
 
-export type SystemScheme = 'light' | 'dark';
-export type SiteScheme = 'light' | 'dark' | 'system';
-
-export type Options = {
-  default: SiteScheme
-}
-
-export type OverrideOptions = {
-  default?: SiteScheme
-}
-
-const QRY = '(prefers-color-scheme: dark)';
 const KEY = Symbol('statekey');
 
-const options: Options = {
-  default: 'system' 
-}
-
-export function createSchemeState(override: OverrideOptions = {}) {
+export function createSchemeState() {
   const inBrowser = typeof window !== 'undefined';
-
-  if (override.default) {
-    options.default = override.default
-  }
-
   return setContext<SchemeState>(KEY, new SchemeState(inBrowser));
 }
 
@@ -36,49 +14,5 @@ export function getSchemeState() {
   }
 
   return getContext<ReturnType<typeof createSchemeState>>(KEY); 
-}
-
-class SchemeState {
-  #systemQuery = new MediaQuery(QRY);
-  #system = $derived<SystemScheme>(this.#systemQuery.current ? 'dark' : 'light');
-  #site = $state<SiteScheme>(options.default);
-  #removeStorageListener:VoidFunction;
-  
-  current = $derived.by(() => {
-      if (this.#site === 'system') {
-        return this.#system;
-      } else {
-        return this.#site;
-      }
-  });
-  
-  constructor(browser:boolean) {
-    if (browser) {
-      this.#site = localStorage.getItem('scheme') as SiteScheme ?? options.default;
-      this.#removeStorageListener = on(window, 'storage', (e: StorageEvent) => {
-        if (e.key === 'scheme') {
-          this.#site = e.newValue as SiteScheme ?? options.default;
-        }          
-      })
-      onDestroy(() => {
-        this.#removeStorageListener();
-      })
-    } else {
-      this.#removeStorageListener = () => {};
-    }
-  }
-
-  set = (v: SiteScheme) => {
-    this.#site = v;
-    localStorage.setItem('scheme', v);
-  }
-
-  get site(): SiteScheme {
-    return this.#site;
-  }
-
-  get system(): SystemScheme {
-    return this.#system;
-  }
 }
 
